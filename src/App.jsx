999fcb0094f5e6fa1b5766c8c046f95e3e65e984
Fanush <fgabriel.mihailescu@gmail.com>
2023-03-14 10:13:03 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <main className="App">
+      <h1>My newest project</h1>
+    </main>
+  )
+}
+
+export default App
